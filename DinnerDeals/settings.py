bbbd4b1c67d111a2d789d0e217bf8869eca74b92
Michael Rohn <sunnykat@me.com>
2016-10-24 13:23:04 -0700

    Added basic urls and apps to settings

--- DinnerDeals/settings.py
+++ DinnerDeals/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.login',
+    'apps.shopping',
+    'apps.recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- apps/shopping/views.py
+++ apps/shopping/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
 
 # Create your views here.
+
+def search(request):
+    return render(request, 'shopping/search.html')
+
+def zipsearch(request):
+    zipp=request.POST['zip']
+    url='http://www.SupermarketAPI.com/api.asmx/StoresByZip?APIKEY=6af0d05f87&ZipCode='+str(zipp)
+    response=requests.get(url)
+    for items in response:
+        print items
+    return response
